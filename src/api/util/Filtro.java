717409d6152d44742a59392558a351342d5764b5
Jonasta <jonastads@gmail.com>
2016-08-18 11:43:08 -0300

    nova versão

--- src/api/util/Filtro.java
+++ src/api/util/Filtro.java
@@ -8,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet Filter implementation class Filtro
@@ -20,35 +19,32 @@ public class Filtro implements Filter {
      * Default constructor. 
      */
     public Filtro() {
+        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see Filter#destroy()
 	 */
 	public void destroy() {
+		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
 
-	    HttpServletResponse res = (HttpServletResponse) response;
-	    
-	    res.setHeader("Access-Control-Allow-Credentials", "true");
-	    res.setHeader("Access-Control-Allow-Origin", "*");
-	    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-	    res.setHeader("Access-Control-Max-Age", "3600");
-	    res.setHeader("Access-Control-Allow-Headers", "Content-Length, Origin, X-Requested-With, Content-Type, Content-Encoding, Accept, Accept-Encoding, Authorization, LockScreenToken");
-
-		chain.doFilter(request, res);
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
 	}
 
 	/**
 	 * @see Filter#init(FilterConfig)
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		
+		// TODO Auto-generated method stub
 	}
 
 }
